387e724c641a468a347b632238a8309ad9cc5bc5
Quinxie51 <quyendoan22@augustana.edu>
2025-06-11 11:19:43 -0700

    Revert "fix: update audio configuration and file paths"
    This reverts commit fe3b2d753c864671abdd695505b85559558f3c1d.

--- code_source/vite.config.ts
+++ code_source/vite.config.ts
@@ -2,21 +2,11 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  assetsInclude: ['**/*.glb', '**/*.gltf', '**/*.bin', '**/*.hdr', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg', '**/*.mp3', '**/*.mp4', '**/*.ico', '**/*.webm', '**/*.json', '**/*.wav', '**/*.ogg'],
+  assetsInclude: ['**/*.glb', '**/*.gltf', '**/*.bin', '**/*.hdr', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg', '**/*.mp3', '**/*.mp4', '**/*.ico', '**/*.webm', '**/*.json'],
   plugins: [],
   server: {
     host: true,
     port: 5500  // required by cors of the dev-env aws server
-  },
-  publicDir: 'public',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
   },
   resolve: {
     alias: {
